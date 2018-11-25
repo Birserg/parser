220879e99734bc7926d22b6a54834bd7972d075d
berserk <s.birtanov@seendex.ru>
2018-11-25 22:24:58 +0300

    added instagram parser

--- anidubparser.py
+++ anidubparser.py
@@ -6,80 +6,77 @@ from multiprocessing import Pool
 
 
 def get_html(url: object) -> object:
-    r = requests.get (url)
+    r = requests.get(url)
     return r.text
 
 
 def get_all_pages(html):
-    soup = BeautifulSoup (html, 'lxml')
-    pages = soup.find ('div', class_='navi').find_all ('a')[-2].text.strip ()
-    return int (pages)
+    soup = BeautifulSoup(html, "lxml")
+    pages = soup.find("div", class_="navi").find_all("a")[-2].text.strip()
+    return int(pages)
 
 
 def get_all_links(html):
-    soup = BeautifulSoup (html, 'lxml')
-    tds = soup.find_all ('div', class_='title')
+    soup = BeautifulSoup(html, "lxml")
+    tds = soup.find_all("div", class_="title")
     links = []
     for td in tds:
-        a = td.find ('a').get ('href')
-        links.append (a)
+        a = td.find("a").get("href")
+        links.append(a)
     return links
 
 
 def get_page_data(html):
-    soup = BeautifulSoup (html, 'lxml')
-    #data = []
+    soup = BeautifulSoup(html, "lxml")
+    # data = []
     try:
-        name = soup.find ('h1', class_='titlfull').text.strip ()
+        name = soup.find("h1", class_="titlfull").text.strip()
     except:
-        name = ''
+        name = ""
 
     try:
-        rate = soup.find ('b', itemprop="ratingValue").text.strip ()
+        rate = soup.find("b", itemprop="ratingValue").text.strip()
         # rate = rate.split('>')[1]
     except:
-        rate = ''
+        rate = ""
 
-    data = {
-        'name': name,
-        'rate': rate
-    }
+    data = {"name": name, "rate": rate}
 
     return data
 
 
 def write_csv(data):
-    with open ('datarate.csv', 'a') as f:
-        writer = csv.writer (f)
-        writer.writerow ((data['name'],data['rate']))
-        print (data['name'], data['rate'], 'parsed')
+    with open("datarate.csv", "a") as f:
+        writer = csv.writer(f)
+        writer.writerow((data["name"], data["rate"]))
+        print(data["name"], data["rate"], "parsed")
 
 
 def make_all(url):
-    html = get_html (url)
-    data = get_page_data (html)
-    write_csv (data)
+    html = get_html(url)
+    data = get_page_data(html)
+    write_csv(data)
 
 
 def main():
-    start = datetime.now ()
-    url = 'https://online.anidub.com/anime_movie/'
-    page_part = 'page/'
-    total_pages = get_all_pages ((get_html (url)))
-    for i in range (1, total_pages + 1):
-        url_gen = url + page_part + str (i) + '/'
-        html = get_html (url_gen)
-        all_links = get_all_links (html)
-        #rint (all_links)
+    start = datetime.now()
+    url = "https://online.anidub.com/anime_movie/"
+    page_part = "page/"
+    total_pages = get_all_pages((get_html(url)))
+    for i in range(1, total_pages + 1):
+        url_gen = url + page_part + str(i) + "/"
+        html = get_html(url_gen)
+        all_links = get_all_links(html)
+        # rint (all_links)
         for u in all_links:
             make_all(u)
-        #with Pool (40) as p:
-         #   p.map (make_all, all_links)
+        # with Pool (40) as p:
+        #   p.map (make_all, all_links)
 
-    end = datetime.now ()
+    end = datetime.now()
     total = end - start
-    print (str (total))
+    print(str(total))
 
 
-if __name__ == '__main__':
-    main ()
+if __name__ == "__main__":
+    main()
